b20461fad28fc67dc7e10985912d85a333466af5
KTanabe3 <m20087@g.metro-cit.ac.jp>
2024-06-03 13:54:05 +0900

    fix files

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, Response, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+DB_USER = "docker"
+DB_PASS = "docker"
+DB_HOST = "db"
+DB_NAME = "flask_app"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.secret_key = "secret"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+
+### テーブル設定 ###############################
+
+#ユーザテーブル
+class User(UserMixin, db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    mail = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(256))
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+### 実装する機能の設定 #########################
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.route('/',methods=['GET'])
+@login_required
+def index_get():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login_get():
+    # 現在のユーザーがログイン済みの場合
+    if current_user.is_authenticated:
+        # トップページに移動
+        return redirect(url_for('index_get'))
+    # loginページのテンプレートを返す
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    user = User.query.filter_by(mail=request.form["mail"]).one_or_none()
+    
+    # ユーザが存在しない or パスワードが間違っている時
+    if user is None or not user.check_password(request.form["password"]):
+        # メッセージの表示
+        flash('メールアドレスかパスワードが間違っています')
+        # loginページへリダイレクト
+        return redirect(url_for('login_get'))
+
+    # ログインを承認
+    login_user(user)
+    # トップページへリダイレクト
+    return redirect(url_for('index_get'))
+
+@app.route('/logout')
+def logout():
+  # logout_user関数を呼び出し
+  logout_user()
+  # トップページにリダイレクト
+  return redirect(url_for('index_get'))
+
+@app.route("/users",methods=['GET'])
+def users_get():
+    users = User.query.all()
+    return render_template('users_get.html', users=users)
+
+@app.route("/users",methods=['POST'])
+def users_post():
+    user = User(
+        name=request.form["user_name"],
+        mail=request.form["mail"]
+    )
+    user.set_password(request.form["password"])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('users_get'))
+
+@app.route("/users/<id>",methods=['GET'])
+@login_required
+def users_id_get(id):
+    if str(current_user.id) != str(id):
+        return Response(response="他人の個別ページは開けません", status=403)
+    user = User.query.get(id)
+    return render_template('users_id_get.html', user=user)
+
+@app.route("/users/<id>/edit",methods=['POST'])
+def users_id_post_edit(id):
+    user = User.query.get(id)
+    user.name = request.form["user_name"]
+    db.session.merge(user)
+    db.session.commit()
+    return redirect(url_for('users_get'))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
